69141e429b1af702ad41395af399cfb22dcf6811
DeMeiring <u17074292@tuks.co.za>
2022-06-07 21:45:01 +0200

    turned discord send endpoint into a post

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const {Client, Intents} = require('discord.js')
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES]});
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
+
+//alows bodyparser as express middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 async function sendDM(message, id){
   try{    
@@ -18,17 +23,19 @@ app.get('/', (req, res) => {
     res.send('Successful response.');
   });
 
-app.get('/sendDM', (req, res) => {
+app.post('/sendDM', (req, res) => {
+  let message = req.body.message;
+  let id = req.body.id;
   client.on('ready', async()=>{
         try{
-          await sendDM(req.query.message, req.query.id);
+          await sendDM(message, id);
           res.send('The message was sent through discord');
         }catch(e){
           res.send('discord encountered an problem: '+e);
         }
     });
     try{
-        sendDM(req.query.message, req.query.id);
+        sendDM(message, id);
         res.send('The message was sent through discord');
     }catch(e){
       res.send('discord encountered an problem: '+e);
